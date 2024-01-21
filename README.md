e1103db77cb8a012946b192d80d42023bc531d23
Chhavi9700225780 <c9081741@gmail.com>
2024-01-21 08:53:51 +0530

    Add a code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Hello-World
 It is my first code in c language
+Radhe radhe
+
---
+++ first.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int s[100];
+    scanf("%[^\n]%*c " , &s);
+    printf("");
+    return 0;
+}
